b374a86ee3f621f0b60a704c0fa0770969589472
Michal Zlamaniec <mcmajk3l@gmail.com>
2012-03-28 12:55:19 +0200

    klasa

--- basic.rb
+++ basic.rb
@@ -2,14 +2,28 @@ require 'sinatra'
 require 'haml'
 require 'json'
 
+class Obliczacz
+	def initialize(dni, cenaPaliwa = 5.61, liczbaKilometrowWJednaStrone=8.8, amortyzacja=1.1)
+		@cenaPaliwa = cenaPaliwa
+		@liczbaKilometrowWJednaStrone = liczbaKilometrowWJednaStrone
+		@amortyzacja = amortyzacja
+		@dni = dni
+	end
+	attr_accessor :cenaPaliwa, :wynik, :dni, :liczbaKilometrowWJednaStrone, :amortyzacja 
+
+	def wynik
+		return (((dni * liczbaKilometrowWJednaStrone * 2) / 19 * cenaPaliwa)*amortyzacja).round
+	end
+end
+
 class MyApp < Sinatra::Base
 	get '/' do  
 	  haml :index  
 	end  
 
 	get '/oblicz/:dni' do
+		oblicz = Obliczacz.new(params[:dni].to_i)
 		content_type :json
-		@dni = params[:dni].to_i
-		dni = (((@dni * 8.8 * 2) / 19 * 5.61)*1.1).round.to_json
+		oblicz.wynik.to_json
 	end
 end
